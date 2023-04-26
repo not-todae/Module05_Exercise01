8b1cf631be1d588cb9dfb8ba0792cfda825c6cde
theredhood427 <bangsil.ronrusselle@auf.edu.ph>
2023-04-26 17:38:44 +0800

    first commit

---
+++ Module05_Exercise01/Module05_Exercise01/DatePicker.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Module05_Exercise01
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DatePicker : ContentPage
+    {
+        public DatePicker()
+        {
+            InitializeComponent();
+        }
+        private void OnDateSelected(object sender, DateChangedEventArgs e)
+        {
+            var selectedDate = e.NewDate;
+        }
+    }
+}
---
+++ Module05_Exercise01/Module05_Exercise01/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+
+namespace Module05_Exercise01
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void DatePicker(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new DatePicker());
+        }
+    }
+}
